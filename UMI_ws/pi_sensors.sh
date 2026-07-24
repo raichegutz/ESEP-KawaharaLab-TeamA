eaf56fde60fc2a4cda0674ab6e6fa2697796f749
raichegutz <raicheg@uci.edu>
2026-07-24 11:33:20 +0900

    modified:   build.sh 	modified:   pi_sensors.sh

--- UMI_ws/pi_sensors.sh
+++ UMI_ws/pi_sensors.sh
@@ -14,8 +14,8 @@ cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq
 echo "=== Launching Core-Pinned Sensor Nodes ==="
 
 # 3. Launch your nodes into their isolated hardware lanes
-chrt -f 85 taskset -c 1 ros2 launch ros2_gelsight_package gelsight_publisher.launch.py &
-chrt -f 90 taskset -c 2 ros2 launch mms101_driver mms101_dual.launch.py &
+taskset -c 1 ros2 launch ros2_gelsight_package gelsight_publisher.launch.py \ enable_compressed:=false &
+taskset -c 2 ros2 launch mms101_driver mms101_dual.launch.py &
 
 
 # Wait for all
