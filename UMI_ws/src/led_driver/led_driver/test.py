cea19d9060107395d46f912d774646c784f80cfd
raichegutz <raicheg@uci.edu>
2026-07-24 16:40:42 +0900

    new file:   src/led_driver/led_driver/test.py 	modified:   src/led_driver/package.xml

---
+++ UMI_ws/src/led_driver/led_driver/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import time
+from gpiozero import LED
+
+GPIO_PIN = 18
+
+led = LED(GPIO_PIN)
+
+print(f"Blinking GPIO {GPIO_PIN}... Press Ctrl+C to stop.")
+
+try:
+    while True:
+        print("ON")
+        led.on()
+        time.sleep(0.5)
+
+        print("OFF")
+        led.off()
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print("\nStopping...")
+finally:
+    led.off()
